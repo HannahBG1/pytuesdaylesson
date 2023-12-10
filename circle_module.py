f8e123ae7549436240ccca80091b618d79408773
Hannah Gettman <hannah.blaser17@gmail.com>
2023-12-10 10:31:09 -0700

    first commit

---
+++ circle_module.py
@@ -0,0 +1,7 @@
+def calculate_circumference():
+    radius = float(input("Enter the radius of the circle: "))
+    circumference = (2*22*radius)/7
+    print("The circumference is ",round(circumference,2)) 
+
+
+
